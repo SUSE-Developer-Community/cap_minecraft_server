1b4fe9523719d8265f4c8023044d1640127ff881
Jeff <jeff.neet@gmail.com>
2020-02-19 12:51:01 -0700

    Update build.sh for Spigot 1.12.2 jar download

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar" -o server.jar
+curl "https://cdn.getbukkit.org/spigot/spigot-1.12.2.jar" -o server.jar
 zip -jur server.jar customizations/*
 
 
