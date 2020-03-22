047a58bbeabaea28a0a221b3814de660fac00feb
Andrew Gracey <andrew.gracey@suse.com>
2020-03-22 12:12:33 -0700

    fixing route guid

--- bin/setupRoutes.sh
+++ bin/setupRoutes.sh
@@ -18,7 +18,7 @@ cf create-route $SPACE cap.explore.suse.dev -n minecraftmap
 
 ROUTES=`cf curl /v2/routes` 
 MAP_ROUTE_GUID= echo ROUTES | jq '.resources[]| select(.entity.host == "minecraftmap") | .metadata.guid'
-SERVER_ROUTE_GUID= echo ROUTES | jq '.resources[]| select(.entity.host == "minecraftserver") | .metadata.guid'
+SERVER_ROUTE_GUID= echo ROUTES | jq '.resources[]| select(.entity.port == 20003) | .metadata.guid'
 
 cf curl /v2/route_mappings -X POST -d "{\"app_guid\": \"${APP_GUID}\", \"route_guid\": ${MAP_ROUTE_GUID}, \"app_port\": 8123}"
 cf curl /v2/route_mappings -X POST -d "{\"app_guid\": \"${APP_GUID}\", \"route_guid\": ${SERVER_ROUTE_GUID}, \"app_port\": 25565}"
